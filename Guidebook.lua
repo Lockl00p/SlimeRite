fb1aca056c8fc7afdf1ac7212d7f8259a95000d9
Lockl00p <97256723+Lockl00p@users.noreply.github.com>
2022-04-26 07:58:58 -0500

    Set up bottom row. Will test.
    Set up bottom row. Will test.

--- Guidebook.lua
+++ Guidebook.lua
@@ -1,7 +1,7 @@
 guidewindow = cLuaWindow(0,9,5,"SlimeRite guide!")
 guidecontents = guidewindow:GetContents()
 
-Bottomrow = cItems()
+
 
 function openguide(plr)
   LOG("Guide opened by ".. plr:GetName())
@@ -15,13 +15,23 @@ function addcategory()
 function initguide()
   
   --First, set up the guide buttons.
+  
+  --Top Row
   --as a refresher, first param for for loop is variable made, second is max num of var, 3rd is increment value (default is one)
   for x = 0,7 do
     guidecontents:SetSlot(x,0,const.emptyitem)
   end
   guidecontents:SetSlot(8,0,cItem(345,1,0))
+  --Top Row End
   
-  
+  --Bottom Row
+  for x = 0,8 do
+    if x == 1 or x == 7 then
+      guidecontents:SetSlot(x,5,160,1,5)
+    else 
+      guidecontents:SetSlot(x,5,const.emptyitem)
+    end
+  end
 end 
 
 function guideclicked(win,plr,SlotNum,ClickAct,Clkitem)
